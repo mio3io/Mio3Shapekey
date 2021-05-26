86b92d21909320ee7e56e8ecd45c6f348323f228
mio3io <2270244+mio3io@users.noreply.github.com>
2021-05-27 07:15:23 +0900

    コレクション合計数が違う問題を修正

--- __init__.py
+++ __init__.py
@@ -68,7 +68,8 @@ class VIEW3D_PT_Mio3sksync(Panel):
                                     "key_blocks", context.object, "active_shape_key_index", rows=3)
 
                 for cobj in context.object.mio3sksync.syncs.objects:
-                    if (cobj.active_shape_key is not None):
+                    if (hasattr(cobj.data, "shape_keys")):
+
                         for ckey in cobj.data.shape_keys.key_blocks:
                             collection_keys.append(ckey.name)
                             if (cobj != context.object):

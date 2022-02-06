7e533f0c8ac6d07a432fa0f4f1d7417f4e75ef47
mio <2270244+mio3io@users.noreply.github.com>
2022-02-07 00:13:05 +0900

    同期方法を変更

--- __init__.py
+++ __init__.py
@@ -1,12 +1,13 @@
 import bpy
 from bpy.types import Panel, PropertyGroup, PointerProperty, UIList, Collection, Object
 from bpy.props import PointerProperty
+from bpy.app.handlers import persistent
 
 bl_info = {
     "name": "Mio3 ShapeKeySync",
     "author": "mio3io",
-    "version": (1, 0, 0),
-    "blender": (2, 80, 0),
+    "version": (2, 0, 0),
+    "blender": (3, 0, 0),
     "warning": "",
     "location": "View3D > Sidebar",
     "description": "Synchronize shape keys with the same name in the certain collection.",
@@ -15,19 +16,22 @@ bl_info = {
 
 
 class MESH_UL_Mio3sksync(UIList):
-
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname, index):
+    def draw_item(
+        self, context, layout, data, item, icon, active_data, active_propname, index
+    ):
 
         obj = active_data
         key_block = item
 
         split = layout.split(factor=0.66, align=False)
-        split.prop(key_block, "name", text="",
-                   emboss=False, icon_value=icon)
+        split.prop(key_block, "name", text="", emboss=False, icon_value=icon)
         row = split.row(align=True)
-        row.emboss = 'NONE'
+        row.emboss = "NONE"
 
-        if key_block.mute or (obj.mode == 'EDIT' and not (obj.use_shape_key_edit_mode and obj.type == 'MESH')):
+        if key_block.mute or (
+            obj.mode == "EDIT"
+            and not (obj.use_shape_key_edit_mode and obj.type == "MESH")
+        ):
             row.active = False
         if not item.id_data.use_relative:
             row.prop(key_block, "frame", text="")
@@ -46,7 +50,13 @@ class VIEW3D_PT_Mio3sksync(Panel):
 
     @classmethod
     def poll(cls, context):
-        return (context.object is not None and context.object.type in {'MESH', 'CURVE', 'SURFACE', 'LATTICE', 'SURFACE'})
+        return context.object is not None and context.object.type in {
+            "MESH",
+            "CURVE",
+            "SURFACE",
+            "LATTICE",
+            "SURFACE",
+        }
 
     def draw(self, context):
         obj = context.object
@@ -54,55 +64,103 @@ class VIEW3D_PT_Mio3sksync(Panel):
         layout = self.layout
         row = layout.row()
         row.label(text="Sync Collection")
-        
-        if (context.object.data.shape_keys):
+
+        if context.object.data.shape_keys:
 
             row.prop(context.object.mio3sksync, "syncs", text="")
 
             collection_keys = []
 
             # コレクション設定済み
-            if (context.object.mio3sksync.syncs):
+            if context.object.mio3sksync.syncs:
                 dat = context.object.data
-                layout.template_list("MESH_UL_Mio3sksync", "", context.object.data.shape_keys,
-                                    "key_blocks", context.object, "active_shape_key_index", rows=3)
+                layout.template_list(
+                    "MESH_UL_Mio3sksync",
+                    "",
+                    context.object.data.shape_keys,
+                    "key_blocks",
+                    context.object,
+                    "active_shape_key_index",
+                    rows=3,
+                )
 
                 for cobj in context.object.mio3sksync.syncs.objects:
-                    if (hasattr(cobj.data, "shape_keys") and cobj.active_shape_key is not None):
-
+                    if (
+                        hasattr(cobj.data, "shape_keys")
+                        and cobj.active_shape_key is not None
+                    ):
                         for ckey in cobj.data.shape_keys.key_blocks:
                             collection_keys.append(ckey.name)
-                            if (cobj != context.object):
-                                if (ckey.name in dat.shape_keys.key_blocks and ckey.value != dat.shape_keys.key_blocks[ckey.name].value):
-                                    ckey.value = dat.shape_keys.key_blocks[ckey.name].value
 
             row = layout.row()
-            row.label(text="Local:" + str(len(context.object.data.shape_keys.key_blocks)))
+            row.label(
+                text="Local:" + str(len(context.object.data.shape_keys.key_blocks))
+            )
             row.label(text="Collection:" + str(len(list(set(collection_keys)))))
 
 
+def callback_update_shapekey(*args):
+    context = bpy.context
+    if context.object.data.shape_keys:
+        if context.object.mio3sksync.syncs:
+            dat = context.object.data
+            for cobj in context.object.mio3sksync.syncs.objects:
+                if (
+                    hasattr(cobj.data, "shape_keys")
+                    and cobj.active_shape_key is not None
+                ):
+                    for ckey in cobj.data.shape_keys.key_blocks:
+                        if cobj != context.object:
+                            if (
+                                ckey.name in dat.shape_keys.key_blocks
+                                and ckey.value
+                                != dat.shape_keys.key_blocks[ckey.name].value
+                            ):
+                                ckey.value = dat.shape_keys.key_blocks[ckey.name].value
+
+
 class MIO3SKSYNC_Props(PropertyGroup):
-    syncs: PointerProperty(name=bpy.app.translations.pgettext("shapekey sync collection"),
-                           type=Collection)
+    syncs: PointerProperty(
+        name=bpy.app.translations.pgettext("shapekey sync collection"), type=Collection
+    )
 
 
-classes = [
-    MIO3SKSYNC_Props,
-    MESH_UL_Mio3sksync,
-    VIEW3D_PT_Mio3sksync
-]
+classes = [MIO3SKSYNC_Props, MESH_UL_Mio3sksync, VIEW3D_PT_Mio3sksync]
+
+
+def register_subscribe_rna():
+    bpy.types.Scene.msgbus_owner = object()
+    subscribe_to = (bpy.types.ShapeKey, "value")
+    bpy.msgbus.subscribe_rna(
+        key=subscribe_to,
+        owner=bpy.types.Scene.msgbus_owner,
+        args=(),
+        notify=callback_update_shapekey,
+    )
+
+
+@persistent
+def handler_subscribe(scene):
+    register_subscribe_rna()
 
 
 def register():
     for c in classes:
         bpy.utils.register_class(c)
     Object.mio3sksync = PointerProperty(type=MIO3SKSYNC_Props)
+    bpy.app.handlers.load_post.append(handler_subscribe)
+    if hasattr(bpy.types.Scene, "msgbus_owner") == False:
+        register_subscribe_rna()
 
 
 def unregister():
-    del Object.mio3sksync
     for c in classes:
         bpy.utils.unregister_class(c)
+    bpy.app.handlers.load_post.remove(handler_subscribe)
+    if hasattr(bpy.types.Scene, "msgbus_owner") == True:
+        bpy.msgbus.clear_by_owner(bpy.types.Scene.msgbus_owner)
+        del bpy.types.Scene.msgbus_owner
+    del Object.mio3sksync
 
 
 if __name__ == "__main__":

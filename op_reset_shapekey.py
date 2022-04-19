b789f69b284d79820e47121904afb152425e152c
mio <2270244+mio3io@users.noreply.github.com>
2022-04-19 14:04:59 +0900

    形状リセットする際確認を出す

--- op_reset_shapekey.py
+++ op_reset_shapekey.py
@@ -41,3 +41,7 @@ class MIO3SK_OT_reset(Operator):
         bpy.ops.object.mode_set(mode=current_mode)
 
         return {"FINISHED"}
+
+    def invoke(self, context, event):
+        wm = context.window_manager
+        return wm.invoke_confirm(self, event)

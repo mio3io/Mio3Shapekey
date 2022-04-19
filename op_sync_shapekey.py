be21c6f8103b7d6e4d6e77117d0717bad71b4961
mio <2270244+mio3io@users.noreply.github.com>
2022-04-19 20:16:09 +0900

    clean

--- op_sync_shapekey.py
+++ op_sync_shapekey.py
@@ -1,22 +1,20 @@
 import bpy
 from .define import *
+from .op_util import *
 
 
 def sync_shapekey_value():
     object = bpy.context.object
-    if object.type not in OBJECT_TYPES or object.mio3sksync.syncs is None:
-        return None
-    shape_keys = object.data.shape_keys
-    for item in object.mio3sksync.syncs.objects:
-        # コレクションアイテム
-        if item.type not in OBJECT_TYPES or item.active_shape_key is None or item == object:
-            continue
-        for item_key in item.data.shape_keys.key_blocks:
-            if item_key.name in shape_keys.key_blocks:
-                if item_key.mute != shape_keys.key_blocks[item_key.name].mute:
-                    item_key.mute = shape_keys.key_blocks[item_key.name].mute
-                if item_key.value != shape_keys.key_blocks[item_key.name].value:
-                    item_key.value = shape_keys.key_blocks[item_key.name].value
+    prop_o = object.mio3sksync
+    if is_sync_collection(object):
+        key_blocks = object.data.shape_keys.key_blocks
+        for item in [v for v in prop_o.syncs.objects if has_shapekey(v) and v != object]:
+            for item_key in item.data.shape_keys.key_blocks:
+                if item_key.name in key_blocks:
+                    if item_key.mute != key_blocks[item_key.name].mute:
+                        item_key.mute = key_blocks[item_key.name].mute
+                    if item_key.value != key_blocks[item_key.name].value:
+                        item_key.value = key_blocks[item_key.name].value
 
 
 msgbus_owner_sync_active_shape_key = object()
@@ -44,25 +42,21 @@ def unregister_active_shape_key():
 
 def sync_active_shape_key():
     object = bpy.context.object
-    if object.type not in OBJECT_TYPES or object.mio3sksync.syncs is None:
-        return None
-    for item in object.mio3sksync.syncs.objects:
-        if item.type not in OBJECT_TYPES or item.active_shape_key is None or item == object:
-            continue
-        index = item.data.shape_keys.key_blocks.find(object.active_shape_key.name)
-        item.active_shape_key_index = index if index >= 0 else 0
+    prop_o = object.mio3sksync
+    if is_sync_collection(object):
+        for item in [v for v in prop_o.syncs.objects if has_shapekey(v) and v != object]:
+            index = item.data.shape_keys.key_blocks.find(object.active_shape_key.name)
+            item.active_shape_key_index = index if index >= 0 else 0
 
 
 # アクティブキーが同期していること
 def sync_rename():
     object = bpy.context.object
-    if object.type not in OBJECT_TYPES or object.mio3sksync.syncs is None:
-        return None
-    for item in object.mio3sksync.syncs.objects:
-        if item.type not in OBJECT_TYPES or item.active_shape_key is None or item == object:
-            continue
-        if item.active_shape_key_index != 0:
-            item.active_shape_key.name = object.active_shape_key.name
+    prop_o = object.mio3sksync
+    if is_sync_collection(object):
+        for item in [v for v in prop_o.syncs.objects if has_shapekey(v) and v != object]:
+            if item.active_shape_key_index != 0:
+                item.active_shape_key.name = object.active_shape_key.name
 
 
 msgbus_owner_auto_active_mirror_edit = object()
@@ -83,15 +77,16 @@ def unregister_auto_active_mirror_edit():
 
 
 def auto_active_mirror_edit():
+    object = bpy.context.object
     prop_s = bpy.context.scene.mio3sk
-    lr_suffix = lr_suffix_types.get(bpy.context.scene.mio3sk.xmirror_auto_suffix_type)
+    lr_suffix = lr_suffix_types.get(prop_s.xmirror_auto_suffix_type)
     if prop_s.xmirror_auto_enabled and bpy.context.active_object.mode == "EDIT":
-        trim_l = bpy.context.object.active_shape_key.name[-lr_suffix[1] :]
-        trim_r = bpy.context.object.active_shape_key.name[-lr_suffix[3] :]
+        trim_l = object.active_shape_key.name[-lr_suffix[1] :]
+        trim_r = object.active_shape_key.name[-lr_suffix[3] :]
         if trim_l == lr_suffix[0] or trim_r == lr_suffix[2]:
-            bpy.context.object.data.use_mirror_x = False
+            object.data.use_mirror_x = False
         else:
-            bpy.context.object.data.use_mirror_x = True
+            object.data.use_mirror_x = True
 
         for win in bpy.context.window_manager.windows:
             [area.tag_redraw() for area in win.screen.areas if area.type in {"VIEW_3D"}]
--- op_util.py
+++ op_util.py
@@ -1,3 +1,9 @@
 import bpy
 from .define import *
 
+
+def is_sync_collection(obj):
+    return obj.type in OBJECT_TYPES and obj.mio3sksync.syncs is not None
+
+def has_shapekey(obj):
+    return obj.type in OBJECT_TYPES and obj.active_shape_key is not None

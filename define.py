39e3a7edd282e39c12cd56fe25ea02f07911e356
mio <2270244+mio3io@users.noreply.github.com>
2022-04-21 18:33:38 +0900

    fix

--- define.py
+++ define.py
@@ -6,7 +6,7 @@ TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "_templates")
 lr_suffix_types_source = [
     ("_head", "_L", "_R"),
     ("upper", "Left", "Right"),
-    ("lower", "left", "Right"),
+    ("lower", "left", "right"),
     ("ja", "左", "右"),
 ]
 lr_suffix_types = {k: (l, len(l), r, len(r)) for (k, l, r) in lr_suffix_types_source}

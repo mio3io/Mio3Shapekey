2d3b8f72d50c1d87765fb7f211fe44b18721ac3d
mio <2270244+mio3io@users.noreply.github.com>
2022-04-26 19:21:29 +0900

    カスタムアイコンの削除

--- icons.py
+++ icons.py
@@ -1,20 +1,26 @@
 import os
 import bpy
 
-ICON_DIR = os.path.join(os.path.dirname(__file__), "icons") 
+ICON_DIR = os.path.join(os.path.dirname(__file__), "icons")
 icons = bpy.utils.previews.new()
 
-def register_icons():
-  icons.load("DEFAULT", os.path.join(ICON_DIR, "default.png"), "IMAGE")
-  icons.load("PRIMARY", os.path.join(ICON_DIR, "primary.png"), "IMAGE")
-  icons.load("PRIMARY_HISTORY", os.path.join(ICON_DIR, "primary_history.png"), "IMAGE")
-  icons.load("MOVE", os.path.join(ICON_DIR, "move.png"), "IMAGE")
 
-  icons.load("FACE_ALL", os.path.join(ICON_DIR, "face_all.png"), "IMAGE")
-  icons.load("FACE_MIRRIR", os.path.join(ICON_DIR, "face_mirror.png"), "IMAGE")
-  icons.load("FACE_L", os.path.join(ICON_DIR, "face_left.png"), "IMAGE")
-  icons.load("FACE_R", os.path.join(ICON_DIR, "face_right.png"), "IMAGE")
+def register_icons():
+    if "DEFAULT" not in icons:
+        icons.load("DEFAULT", os.path.join(ICON_DIR, "default.png"), "IMAGE")
+        icons.load("PRIMARY", os.path.join(ICON_DIR, "primary.png"), "IMAGE")
+        icons.load("PRIMARY_HISTORY", os.path.join(ICON_DIR, "primary_history.png"), "IMAGE")
+        icons.load("MOVE", os.path.join(ICON_DIR, "move.png"), "IMAGE")
+
+        icons.load("FACE_ALL", os.path.join(ICON_DIR, "face_all.png"), "IMAGE")
+        icons.load("FACE_MIRRIR", os.path.join(ICON_DIR, "face_mirror.png"), "IMAGE")
+        icons.load("FACE_L", os.path.join(ICON_DIR, "face_left.png"), "IMAGE")
+        icons.load("FACE_R", os.path.join(ICON_DIR, "face_right.png"), "IMAGE")
+
 
 def remove_icons():
-  if "FACE_ALL" in icons:
-    bpy.utils.previews.remove(icons)
+    for pcoll in icons.values():
+        if pcoll in icons:
+            bpy.utils.previews.remove(pcoll)
+    icons.clear()
+    pass
